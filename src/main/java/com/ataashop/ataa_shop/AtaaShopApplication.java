7016bac8b9f4d47c85fbb1c87648b3f472d3f019
Ataa <mohd.ataa.khan@sap.com>
2025-03-04 15:52:39 +0530

    first commit

---
+++ src/main/java/com/ataashop/ataa_shop/AtaaShopApplication.java
@@ -0,0 +1,13 @@
+package com.ataashop.ataa_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtaaShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtaaShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ataashop/ataa_shop/AtaaShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ataashop.ataa_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtaaShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
